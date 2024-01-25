a5b32fc9869a9a9ac97cf30e6532bd0d04e9ed1e
nabil alaa <nabilalaa06@gmail.com>
2024-01-25 14:53:12 +0200

    blog

--- config/plugins.js
+++ config/plugins.js
@@ -7,6 +7,7 @@ module.exports = ({ env }) => ({
         cloud_name: env("CLOUDINARY_NAME"),
         api_key: env("CLOUDINARY_KEY"),
         api_secret: env("CLOUDINARY_SECRET"),
+        upload_preset: "sport-blog"
       },
       actionOptions: {
         upload: {
